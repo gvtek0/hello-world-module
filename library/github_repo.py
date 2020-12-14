9066f84aeefc2f9447b2b5cb6382e4ce7205287c
gvtek0 <gvtek0@gmail.com>
2020-12-13 23:30:28 -0800

    formatting

--- library/github_repo.py
+++ library/github_repo.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-from ansible module_utils basic import *
+from ansible.module_utils.basic import *
+
 def main():
 
-    module = AnsibleModule(argument_spec())
-    response - ("Hello" : "World" )
-    module.exit_json(changed=False, meta=response)
+	module = AnsibleModule(argument_spec={})
+	response = {"hello": "world"}
+	module.exit_json(changed=False, meta=response)
 
 
 if __name__ == '__main__':
